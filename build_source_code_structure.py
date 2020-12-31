86631ae9577079c8e360485732d9e2e2687dcbec
Eric Tapia <erictapia1@gmail.com>
2020-12-30 20:18:34 -0800

    Corrected variable name directory to dir.  Day directories now start at 1 and are 0 padded.

--- build_source_code_structure.py
+++ build_source_code_structure.py
@@ -4,6 +4,7 @@ import os
 root_path = './source_code'
 directory_prefix = 'day'
 days = 100
+padding = '03' # Max 3 digits or padd with 0
 
 readme_filename = "README.md"
 
@@ -34,7 +35,7 @@ paste output here
 def create_directory(dir):
     # Create directory if does not exist
     if not os.path.isdir(dir):
-        os.mkdir(directory)
+        os.mkdir(dir)
 
 def create_file(filename, content):
 
@@ -51,13 +52,17 @@ def build():
     # Create root path if does not exist
     create_directory(root_path)
 
-    for day in range(days):
+    for day in range(1, days+1): # Start on 1
         # Create directory
-        directory = f'{root_path}/{directory_prefix}{day}'       
+        padded_day = f'{day:{padding}}'
+        directory = f'{root_path}/{directory_prefix}{padded_day}'       
         create_directory(directory)
         
         # Create README.md file
         filename = f'{directory}/{readme_filename}'
+
+        # Add padding
+        padded_day = f'{day:{padding}}'
         create_file(filename, readme_contents.format(day))   
 
 if __name__ == '__main__':
