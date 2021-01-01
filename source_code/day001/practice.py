b05a44493e3ae95bf1385e714b3f6f86f67eaba2
avholloway <avholloway@gmail.com>
2021-01-01 15:58:29 -0600

    added day 1 practice tasks

---
+++ source_code/day001/practice.py
@@ -0,0 +1,20 @@
+# 1.1
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# 1.2
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the \"+\" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# 1.3
+name = input("name: ")
+print(f"{len(name)=}")
+
+# 1.4
+a = input("a: ")
+b = input("b: ")
+a, b = b, a
+print(f"{a=}\n{b=}")
