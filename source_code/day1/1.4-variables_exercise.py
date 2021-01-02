664a9e6e6e21c12e5ad294e72c2dbe53db693132
Robin Canela <rcanela1@gmail.com>
2021-01-01 23:59:09 -0500

    day1

--- source_code/day1/1.4-variables_exercise.py
+++ source_code/day1/1.4-variables_exercise.py
@@ -5,11 +5,9 @@ b = input("b: ")
 
 ####################################
 #Write your code below this line 👇
-
 c = a
 a = b
 b = c
-
 #Write your code above this line 👆
 ####################################
 
--- source_code/day1/main.py
+++ source_code/day1/main.py
@@ -17,6 +17,7 @@ length = len(name)
 print(length)
 
 # Day-1-printing end
+
 # input() will get user input in console
 # Then print() will print the word "Hello" and the user input
 print("Hello " + input("What is your name?"))
