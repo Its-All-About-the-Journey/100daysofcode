6af1934137238bc0c2df0f6920bdf60bc9e9cfe1
Angel Inglese V <angel.inglese@gmail.com>
2021-01-02 13:22:23 -0500

    Addind project Day 1

---
+++ source_code/day001/day-1-project.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Greetings! Let's create a band name")
+#2. Ask the user for the city that they grew up in.
+city = input("What's the name of your city?\n")
+#3. Ask the user for the name of a pet.
+pet_name = input("What's the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+band_name = city + " " + pet_name
+print (" your band name is: " + band_name)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
