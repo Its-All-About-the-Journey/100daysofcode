b2233e34baddcc6b349c3ea40331d01cff86c444
Robin Canela <rcanela1@gmail.com>
2021-01-01 22:39:13 -0500

    day 1, 3 exerices

--- source_code/day1/1.2-debugging_exercise.py
+++ source_code/day1/1.2-debugging_exercise.py
@@ -1,3 +1,4 @@
+# Fix the code below
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
---
+++ source_code/day1/main.py
@@ -0,0 +1,6 @@
+print("Hello" + " Robin")
+print("what is your name?")
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name: "))
