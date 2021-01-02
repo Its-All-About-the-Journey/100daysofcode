8552063bcae68c9792c0362cb8102d1c86ec8c92
Angel Inglese V <angel.inglese@gmail.com>
2021-01-02 13:06:43 -0500

    adding excercise 4 - day 1

---
+++ source_code/day001/day-1-4-exercise.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+pivot = a
+a = b
+b = pivot
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
