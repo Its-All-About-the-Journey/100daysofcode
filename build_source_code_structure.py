f678edcbdedf1c91336e4756f3fe46424d0053b6
Eric Tapia <erictapia1@gmail.com>
2021-01-01 21:33:10 -0800

    Updated source code structure to source_code/dayxxx/username/.

--- build_source_code_structure.py
+++ build_source_code_structure.py
@@ -1,5 +1,7 @@
 import os
+import shutil
 
+username = 'test'  # <----------------------CHANGE THIS
 
 root_path = './source_code'
 directory_prefix = 'day'
@@ -8,13 +10,19 @@ padding = '03' # Max 3 digits or padd with 0
 
 readme_filename = "README.md"
 
+# The readme_contents will be used a template and the 
+# script will create a README.md for all day directory.  
+# Change sections that will be repeated for each day
+# such as OS, Python ver.
+# You can also additional section of your choosing.
+
 readme_contents = '''
 # DAY {}
 
 # Description
 
 # Environment
-OS:
+OS: Yes
 
 Python version:
 
@@ -33,22 +41,32 @@ paste output here
 
 
 def create_directory(dir):
-    # Create directory if does not exist
+    # Create directory if one does not exist
     if not os.path.isdir(dir):
         os.mkdir(dir)
 
-def create_file(filename, content):
+def create_file(filename, content, overwrite=False):
 
     if not os.path.isfile(filename):
         mode = 'w+' # Non existing file
+        file_exists = False
     else:
         mode = 'w' # Exists
+        file_exists = True
     
-    with open(filename, mode) as file:
-        file.write(content)
+    # file_exists |  overwrite
+    #-----------------------
+    #   T         |   T          <- Overwrite file
+    #   T         |   F          <- Do not touch file
+    #   F         |   T          <- Create file
+    #   F         |   F          <- Create file
+    if not file_exists & ~overwrite:
+        with open(filename, mode) as file:
+            file.write(content)
 
 
-def build():
+def build(overwrite_readme):
+
     # Create root path if does not exist
     create_directory(root_path)
 
@@ -58,9 +76,43 @@ def build():
         directory = f'{root_path}/{directory_prefix}{padded_day}'       
         create_directory(directory)
         
+        # Update 1 - Adding username directory so there is no conflict when
+        # each user merges thier branch to the main branch
+        create_directory(f'{directory}/{username}')
+
+        # Move all files in the day directory to the new username directory
+        file_list = os.listdir(directory)
+
+        for file in file_list:
+            src = f'{directory}/{file}'
+            dest = f'{directory}/{username}'
+            shutil.move(src, dest, copy_function=shutil.copytree)
+
         # Create README.md file
-        filename = f'{directory}/{readme_filename}'
-        create_file(filename, readme_contents.format(day))   
+        filename = f'{directory}/{username}/{readme_filename}'
+        create_file(filename, readme_contents.format(day), overwrite_readme)
 
 if __name__ == '__main__':
-    build()
+    
+    # Exit if default username has not been changed
+    if username == 'CHANGEME':
+        print(f'You forgot to change the username variable in the python script.')
+        print(f'It is currently set to {username}')
+    
+    else:
+        print('*'*80)
+        print(f'WARNING: It will overwrite all {readme_filename} file inside {root_path} directory')
+        print('*'*80)
+        print('Do you want overwrite each README.md?')
+        print("Enter yes to overwrite or press enter to keep existing")
+        overwrite_readme = input()
+        print()
+
+        if overwrite_readme == 'yes':
+            print(f'Overwriting any existing README.md in the {root_path}')
+            build(True)
+        else:
+            print('Keeping any existing README.md.')
+            build(False)
+        
+        print('*'*80)
