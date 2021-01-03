ad0be7c7b8f358e8d8bef5ba6871ccb603f1e06f
Trynius <jmiller@hazeldenbettyford.org>
2021-01-03 09:20:54 -0600

    Day 2 Completed

--- source_code/day002/README.md
+++ source_code/day002/README.md
@@ -2,6 +2,7 @@
 # DAY 2
 
 # Description
+The final exercise of this day was creating a tip calculator. One thing this module reminded me which wasn't represented in the final project was accessing string elements by index (IE: every character in a string can be accessed by index). I also implemented data validation into the final code just for fun.
 
 # Environment
 OS: Windows 10
@@ -9,13 +10,18 @@ OS: Windows 10
 Python version: 3.9.1
 
 # Dependencies
+N/A
 
 # How to run script
 ```
-enter instructions here
+python tip-calculator.py
 ```
 
 # Sample output
 ```
-paste output here
+Welcome to the tip calculator.
+What was the bill total? 124.75
+How many people are splitting the bill? 5
+What percentage tip would you like to give: 10, 12, or 15? 10
+Each person should pay: $27.45
 ```
---
+++ source_code/day002/tip-calculator.py
@@ -0,0 +1,38 @@
+print("Welcome to the tip calculator.")
+
+# Get the bill total, check for valid input.
+while True:
+    try:
+        total = float(input("What was the bill total? $"))
+        break
+    except ValueError:
+        print("Please enter the bill total in decimal format (ie: 100.00)")
+
+# Get the number of people, check for valid integer input.
+while True:
+    try:
+        people = int(input("How many people are splitting the bill? "))
+        break
+    except ValueError:
+        print("Please enter a whole number.")
+
+# Set valid values and boolean.
+percentageOptions = [10, 12, 15]
+validPercentage = False
+
+# Get the tip percentage, check for valid input (10, 12, or 15).
+while not validPercentage:
+    try:        
+        tip_percent  = int(input("What percentage tip would you like to give: 10, 12, or 15? "))
+        if tip_percent == 10 or tip_percent == 12 or tip_percent == 15:
+            break
+        else:
+            print('You must enter 10, 12, or 15.')
+    except:
+        print('You must enter 10, 12, or 15.')
+    
+tip = total * (tip_percent/100)
+total += tip
+
+# Print payment per-person in 2-decimal format.
+print("Each person should pay: $" + str(format(total / people, '.2f')))
