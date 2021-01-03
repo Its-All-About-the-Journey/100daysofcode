05392c4d91c40185d060e32fe6ad2c1bda194557
Angel Inglese V <angel.inglese@gmail.com>
2021-01-03 11:55:39 -0500

    Adding a few daily challenges

---
+++ source_code/day003/day-3-1-exercise.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+modulo = number % 2
+if modulo == 0:
+  print(f"{number} is an even number")
+else:
+  print(f"{number} is an odd Number")
+  
---
+++ source_code/day003/day-3-start.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height >= 120:
+  print("you can pass to the rollercoaster!")
+else:
+  print("you cannot pass, sorry")
