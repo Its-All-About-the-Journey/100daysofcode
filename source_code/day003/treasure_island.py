009241d0148e7e624ba35a1b672fbc34e651e8b8
Danny Wade <danny.wade35@gmail.com>
2021-01-03 18:17:39 -0500

    added day 3 project - treasure island

---
+++ source_code/day003/treasure_island.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+# https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+crossroad = input(
+    "You're at a crossroad. Do you want to go right or left?\n").lower()
+if crossroad == 'left':
+    swim = input(
+        "You need to get to the island. Would you like to swim or wait for a boat?\n").lower()
+    if swim == 'wait':
+        door = input(
+            "You are presented with three doors: red, blue, yellow. Which color door would you like to go through?\n").lower()
+        if door == 'red':
+            print("You were burned by fire. Game over.\n")
+        elif door == 'blue':
+            print("You were eaten by beasts. Game over.\n")
+        elif door == 'yellow':
+            print("You win!")
+        else:
+            print("Game over.")
+    else:
+        print("You were attacked by a trout. Game over.\n")
+else:
+    print("You fell into a hole. Game over.\n")
