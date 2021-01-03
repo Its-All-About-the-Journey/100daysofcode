121e00488c9ac8ca9756eb19d10a2469cfbd443c
Robin Canela <rcanela1@gmail.com>
2021-01-03 11:05:02 -0500

    update sec3.27

--- source_code/day3/sec3.27.py
+++ source_code/day3/sec3.27.py
@@ -1,7 +1,7 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
-if height > 120:
+if height >= 120:
     print("You can ride the rollercoaster!")
 else:
-    print("Sorry, you are not tall enough to ride the roller coaster.")
+    print("Sorry, you are not tall enough to ride the roller coaster!")
