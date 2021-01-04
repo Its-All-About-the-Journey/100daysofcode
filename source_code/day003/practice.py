e258dfc06b894aa69ce85027f2f0941dbd5055f6
avholloway <avholloway@gmail.com>
2021-01-03 23:39:09 -0600

    finished all exercises

--- source_code/day003/practice.py
+++ source_code/day003/practice.py
@@ -6,7 +6,7 @@ number = int(input("Which number do you want to check? "))
 
 #Write your code below this line 👇
 
-print(f"This is an {'odd' if number % 2 else 'even'} number")
+print(f"This is an {'odd' if number % 2 else 'even'} number")}
 
 
 # 3.2 - bmi calc 2.0
@@ -24,15 +24,15 @@ bmi = round(float(weight) / (float(height) ** 2))
 print(f"Your BMI is {bmi}, ", end="")
 
 if bmi < 18.5:
-  print("you are underweight.")
+print("you are underweight.")
 elif bmi < 25:
-  print("you have a normal weight.")
+print("you have a normal weight.")
 elif bmi < 30:
-  print("you are slightly overweight.")
+print("you are slightly overweight.")
 elif bmi < 35:
-  print("you are obese.")
+print("you are obese.")
 else:
-  print("you are clinically obese.")
+print("you are clinically obese.")
 
 # 3.3 - leap year
 # ---------------------------------------------------------
@@ -46,11 +46,30 @@ year = int(input("Which year do you want to check? "))
 # except if also evenly divisible by 100
 # unless if also evenly divisible by 400
 
+# golfed example which passes 3.3 testing
+# print(f"{'Leap year.' if not (year % (4 if year % 25 else 16)) else 'Not leap year.'}")
+
 evenly_divisible_by_4 = not year % 4
 evenly_divisible_by_100 = not year % 100
 evenly_divisible_by_400 = not year % 400
 
-if not evenly_divisible_by_4 or (evenly_divisible_by_100 and not evenly_divisible_by_400) :
-  print(f"Not leap year.")
+if evenly_divisible_by_4 and (not evenly_divisible_by_100 or evenly_divisible_by_400) :
+print("Leap year.")
 else:
-  print("Leap year.")
+print("Not leap year.")
+
+# 3.4 - pizza order
+# ---------------------------------------------------------
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+cost = {"S": 15, "M": 20, "L": 25}.get(size, 0)
+cost += {"YS": 2, "YM": 3, "YL": 3}.get(add_pepperoni + size, 0)
+cost += {"YS": 1, "YM": 1, "YL": 1}.get(extra_cheese + size, 0)
+print(f"Your final bill is: ${cost}.")
