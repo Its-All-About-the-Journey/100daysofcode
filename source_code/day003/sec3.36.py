ddd54e3b19a7adc2811b5d4deacbbcf7755545a2
Eric Tapia <erictapia1@gmail.com>
2021-01-04 17:48:29 -0800

    Added section 3.36, Treaure Island.

---
+++ source_code/day003/sec3.36.py
@@ -0,0 +1,52 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+
+
+print('='*80)
+if input('Which direction do you want to go, LEFT or RIGHT? ').lower() != 'left':
+    print('\nGame Over')
+
+elif input('\nThe path to the next door may require swimming.\nDo you want to SWIM or WAIT? ').lower() != 'wait':
+    print('\nGame Over')
+
+elif  input('\nWAIT a second, there is a RED and a BLUE door.  Which door do you want to open? ').lower() != 'wait': 
+    print('\nGame Over')
+
+else:
+    print('\nYou see a sign.')
+    print('='*80)
+    print('Confucius says, actually I do not know what Confucius says but your decisions saved')
+    print('your life!')
+    print()
+    print('Congratulations you are a winner but your are also a loser!')
+    print('Did you really think there was a treasure?')
+    print()
+    print('GAME OVER! MUUUAAAHAHHAHAHA')
+    print()
+
+print('='*80)
