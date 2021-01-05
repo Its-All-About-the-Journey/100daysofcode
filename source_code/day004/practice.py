88350ef71a15967428fa443489dd8f1eb4502246
avholloway <avholloway@gmail.com>
2021-01-04 23:37:49 -0600

    4.3 done

--- source_code/day004/practice.py
+++ source_code/day004/practice.py
@@ -29,3 +29,30 @@ from random import randint
 who = randint(0, len(names) - 1)
 
 print(f"{names[who]} pays")
+
+# 4.3 - treasure map
+# ---------------------------------------
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+# take 2-digit input as string
+# split each digit into own list element
+# converting each to int
+# and subtract 1 from each for 0-indexing
+col, row = [int(p) - 1 for p in list(position)]
+
+# place marker on map
+map[row][col] = "X"
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
