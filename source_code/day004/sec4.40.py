4b7900be858ea033f2ab4a9a843eb1ec61d4ef3a
Eric Tapia <erictapia1@gmail.com>
2021-01-05 07:08:49 -0800

    Added section 4.40, Heads or Tails.

---
+++ source_code/day004/sec4.40.py
@@ -0,0 +1,8 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+coin_face = 'Heads' if random.randint(0,1) else 'Tails'
+
+print(coin_face)
