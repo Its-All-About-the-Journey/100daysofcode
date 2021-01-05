d62bb3ee30c9bd24a0c10a1afa3495556e34e02e
Angel Inglese V <angel.inglese@gmail.com>
2021-01-05 14:40:40 -0500

    excersice 1 day 4

---
+++ source_code/day004/day-4-1-exercise.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+random_int = random.randint(0,1)
+
+if random_int == 1:
+  print("Head")
+else:
+  print("Tail")
+
--- source_code/day004/day-4-start.py
+++ source_code/day004/day-4-start.py
@@ -2,3 +2,8 @@ import random
 
 random_int = random.randint(1,200)
 print(random_int)
+
+random_float = random.random()
+print(random_float)
+random_num = random_int + random_float
+print(random_num)
