8d30ec38bc4d1045cfbbff46ca73ad16bcd17e08
Eric Tapia <erictapia1@gmail.com>
2021-01-05 07:56:31 -0800

    Added section 4.43, Treasure Map.

---
+++ source_code/day004/sec4.43.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+# Assumes 2 digits, - 1 due to 0 indexing
+# Per instructions input 23 is col:2 row:3
+col = int(position[0]) - 1
+row = int(position[1]) - 1
+
+map[row][col] = 'X'
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
