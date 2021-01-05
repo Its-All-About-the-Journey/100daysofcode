2f2dc849de1cb04714739e3f9690c77737ae46bd
Angel Inglese V <angel.inglese@gmail.com>
2021-01-04 19:45:14 -0500

    sorry

--- source_code/day003/day-3-treasure-island.py
+++ source_code/day003/day-3-treasure-island.py
@@ -11,8 +11,6 @@ print('''
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
-
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 direction = input("left or right?\nAnswer: ")
 direction_lc = direction.lower()
 if direction_lc == "left":
