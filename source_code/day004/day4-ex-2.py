f6ca0192c67d7ed6d68f9177ba0da541ad983beb
A.J. Murray <noblinkyblinky@gmail.com>
2021-01-04 21:31:45 -0500

    Day 4 ex 2

---
+++ source_code/day004/day4-ex-2.py
@@ -0,0 +1,16 @@
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+import random
+
+x = len(names) - 1
+
+shake = random.randint(0,x)
+
+who_pays = names[shake]
+
+print(who_pays)
+
