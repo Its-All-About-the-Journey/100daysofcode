d91f5dcf57e6a3c19bd32ae8c7ec75c0ce029eed
Robin Canela <rcanela1@gmail.com>
2021-01-04 23:31:54 -0500

    beginning day 4

---
+++ source_code/day4/sec4.39.py
@@ -0,0 +1,13 @@
+import random
+
+randomInteger = random.randint(1, 10)
+print(randomInteger)
+
+randomFloat = random.random()
+print(randomFloat)
+
+randomFloat = random.random() * 5
+print(randomFloat)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}.")
