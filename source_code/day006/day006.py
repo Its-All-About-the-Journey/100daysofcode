960e8125d67b20efc760fda97853de0f361f754c
Armstrong <grantarm@buffalo.edu>
2021-01-06 13:57:42 -0500

    Updated Day006 Maze Code

--- source_code/day006/day006.py
+++ source_code/day006/day006.py
@@ -2,6 +2,8 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
+while not is_facing_north() and right_is_clear() :
+    turn_left()
 
 while not at_goal():
     if is_facing_north() and not wall_in_front():
