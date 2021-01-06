ec6c36ab3a94ad55b75cf43f989a2f65a64e7e1b
avholloway <avholloway@gmail.com>
2021-01-05 18:34:19 -0600

    5.2 done

--- source_code/day005/practice.py
+++ source_code/day005/practice.py
@@ -17,3 +17,19 @@ average_height = round(total_height / i)
 
 print(f"Average student height is {average_height}")
 
+# 5.2 - highest score
+# --------------------------------------
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+high_score = 0
+for score in student_scores:
+  if score > high_score:
+    high_score = score
+print(f"The highest score in the class is: {high_score}")
