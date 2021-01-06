2c50fc6298a109ce1a7064220c7da8c43ceb2cb9
avholloway <avholloway@gmail.com>
2021-01-05 18:47:11 -0600

    5.4 done

--- source_code/day005/practice.py
+++ source_code/day005/practice.py
@@ -43,3 +43,17 @@ for i in range(1, 101):
   if i % 2 == 0:
     total += i
 print(total)
+
+# 5.4 - fizzbuzz
+# --------------------------------------
+#Write your code below this row 👇
+
+for i in range(1, 101):
+if i % 15 == 0:
+    print("FizzBuzz")
+elif i % 3 == 0:
+    print("Fizz")
+elif i % 5 == 0:
+    print("Buzz")
+else:
+    print(i)
