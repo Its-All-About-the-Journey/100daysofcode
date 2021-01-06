3a555ec32c42a0406f97230ec6005da569710fa9
A.J. Murray <noblinkyblinky@gmail.com>
2021-01-06 14:43:02 -0500

    Day 5 ex 3 Even Numbers

---
+++ source_code/day005/ex5-3.py
@@ -0,0 +1,8 @@
+#Write your code below this row 👇
+
+total = 0
+
+for n in range(1,101):
+  if n % 2 == 0:
+    total += n
+print(total)
---
+++ source_code/day005/gauus.py
@@ -0,0 +1,7 @@
+total = 0
+
+for number in range(1, 101):
+    total += number
+
+
+#print(total)
