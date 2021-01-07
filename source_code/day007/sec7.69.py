3ca0f539b32a2970c5c3542f21d0b0e3145fd770
Eric Tapia <erictapia1@gmail.com>
2021-01-07 08:36:52 -0800

    Corrected the for loop. Range(X) iterates 0 - (x-1).

--- source_code/day007/sec7.69.py
+++ source_code/day007/sec7.69.py
@@ -21,7 +21,7 @@ guess = input("Guess a letter: ").lower()
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for index in range(num_chars - 1):
+for index in range(num_chars):
     if guess.lower() == chosen_word[index]:
         display[index] = guess.lower()
 
