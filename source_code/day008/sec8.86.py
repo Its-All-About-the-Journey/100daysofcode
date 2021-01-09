5a8874022883c1115d00a61f0c06f4e2dfac7287
Eric Tapia <erictapia1@gmail.com>
2021-01-08 20:41:47 -0800

    I forgot to do step 3.  Now it's complete.

--- source_code/day008/sec8.86.py
+++ source_code/day008/sec8.86.py
@@ -1,19 +1,26 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(start_text, shift_amount, cipher_direction):
-  end_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-  for char in start_text:
-    #TODO-3: What happens if the user enters a number/symbol/space?
-    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-    #e.g. start_text = "meet me at 3"
-    #end_text = "•••• •• •• 3"
-    position = alphabet.index(char)
-    new_position = position + shift_amount
-    end_text += alphabet[new_position]
-    
-  print(f"Here's the {cipher_direction}d result: {end_text}")
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+
+    for char in start_text:
+        #TODO-3: What happens if the user enters a number/symbol/space?
+        #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+        #e.g. start_text = "meet me at 3"
+        #end_text = "•••• •• •• 3"
+
+        # Only shift if a - z, 
+        if ord(char) < ord('a') or ord(char) > ord('z'):
+          end_text += char
+        else:
+
+          position = alphabet.index(char)
+          new_position = position + shift_amount
+          end_text += alphabet[new_position]
+      
+    print(f"Here's the {cipher_direction}d result: {end_text}")
 
 #TODO-1: Import and print the logo from art.py when the program starts.
 from art import logo
@@ -41,7 +48,7 @@ while run:
         shift %= 26
             
         caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-    
+  
     else:
         run = False
 
