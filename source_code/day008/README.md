551e48ec26ceb184078a5bd4d2592d05f5102821
grantarm <73594868+grantarm@users.noreply.github.com>
2021-01-09 22:44:38 -0500

    Day008 Caesar Cipher

---
+++ source_code/day008/README.md
@@ -0,0 +1,59 @@
+
+# DAY 8
+
+# Description
+Caesar Cipher Python Program - I decided to design it slightly differently than recommended. 
+I hope you enjoy it!
+
+# Environment
+OS: Windows 10
+
+Python version: 3.8
+
+# Dependencies
+None
+
+# How to run script
+```
+No Special Instructions
+```
+
+# Sample output
+```
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+
+Type 'encode' to encrypt, type 'decode' to decrypt:
+encode
+Type your message:
+Super secret message! Do not read this!!
+Type the shift number:
+56
+The encodeed text is: wytiv wigvix qiwweki! hs rsx vieh xlmw!!
+Would you like to run the program again (yes/no)?yes
+Type 'encode' to encrypt, type 'decode' to decrypt:
+decrypt
+Type your message:
+wytiv wigvix qiwweki! hs rsx vieh xlmw!!
+Type the shift number:
+56
+The decrypted text is: super secret message! do not read this!!
+Would you like to run the program again (yes/no)?no
+
+ Goodbye User!
+
+Process finished with exit code 0
+```
---
+++ source_code/day008/day008.py
@@ -0,0 +1,61 @@
+'''
+Grant Armstrong
+1/9/2020
+Caesar Cipher Day 8 Project
+'''
+
+
+from art import logo
+
+print(logo)
+run_again = True
+
+# This cipher converts between the unicode point representations of characters
+# to encode and decode the text
+def caesar(text, shift, direction):
+    if shift > 26:
+        shift = shift % 26
+    text = list(text)
+    text_list = []
+    for char in text:
+        if 32 <= ord(char) <= 64:
+            text_list.append(char)
+        elif direction.lower() in ['encode', 'encrypt']:
+            if ord(char) + shift <= 122:
+                text_list.append(chr(ord(char) + shift))
+            elif ord(char) + shift > 122:
+                adjusted_unicode = 96 + (ord(char) + shift - 122)
+                text_list.append(chr(adjusted_unicode))
+        elif direction.lower() in ['decode', 'decrypt']:
+            if ord(char) - shift >= 97:
+                text_list.append(chr(ord(char) - shift))
+            elif ord(char) - shift < 97:
+                adjusted_unicode = 123 - (97 - (ord(char) - shift))
+                text_list.append(chr(adjusted_unicode))
+    code_text = ''.join(text_list)
+    print(f'The {direction}ed text is: {code_text}')
+
+
+def run_caesar():
+
+    while True:
+        direction_input = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        if direction_input in ['encode', 'encrypt', 'decode', 'decrypt']:
+            break
+        else:
+            print(f'"{direction_input}" is an invalid selection. Please try again...')
+            continue
+
+    text_input = input("Type your message:\n").lower()
+    shift_input = int(input("Type the shift number:\n"))
+    caesar(text_input, shift_input, direction_input)
+
+
+while run_again:
+    run_caesar()
+    again = input("Would you like to run the program again (yes/no)?")
+    if again.lower() in ['no', 'n']:
+        run_again = False
+        print('\n', 'Goodbye User!')
+    elif again.lower() in ['yes', 'y']:
+        run_again = True
