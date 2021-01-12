b56ea0485afec29175259add64065c18b01307ba
Robin Canela <rcanela1@gmail.com>
2021-01-12 15:27:40 -0500

    added all files to day 5, completed sec5.48.py

---
+++ source_code/day5/sec5.48.py
@@ -0,0 +1,8 @@
+# Using the for loop with Python Lists
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+    print("I love to eat " + fruit + ".")
+    print(fruit + " Pie")
+    print(fruits)
+print(fruits)
