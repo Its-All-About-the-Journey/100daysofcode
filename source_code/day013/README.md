bcebac007d1c0184df2ca86c95e9dea53eef2db2
avholloway <avholloway@gmail.com>
2021-01-13 15:05:10 -0600

    day 13 complete

--- source_code/day013/README.md
+++ source_code/day013/README.md
@@ -2,20 +2,22 @@
 # DAY 13
 
 # Description
+Debugging day!  This was light work.  Didn't learn anything new, but it's 
+good to be coding nonetheless.
 
 # Environment
-OS:
-
-Python version:
+Using repl.it
+Python version: 3.8.2
 
 # Dependencies
+repl.it account
 
 # How to run script
 ```
-enter instructions here
+N/A
 ```
 
 # Sample output
 ```
-paste output here
+N/A
 ```
---
+++ source_code/day013/practice.py
@@ -0,0 +1,82 @@
+############DEBUGGING#####################
+
+# Describe Problem: the if block is waiting for i to be 20, but it will never be, since rang() will stop at 19
+# def my_function():
+#   for i in range(1, 21):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+# Reproduce the Bug: 1 to 6 randint, but indexes are 0 to 5, so index 6 is out of range
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
+
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year >= 1994:
+#   print("You are a Gen Z.")
+
+# # Fix the Errors
+# age = int(input("How old are you? "))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page = int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+# #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#     b_list.append(new_item)
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
+
+# 13.1
+# -----------------------------------------------------------------------------
+def one():
+    number = int(input("Which number do you want to check?"))
+    if number % 2 == 0:
+        print("This is an even number.")
+    else:
+        print("This is an odd number.")
+
+# 13.2
+# -----------------------------------------------------------------------------
+def two():
+    year = int(input("Which year do you want to check?"))
+    
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year.")
+            else:
+                print("Not leap year.")
+        else:
+            print("Leap year.")
+    else:
+        print("Not leap year.")
+
+# 13.3
+# -----------------------------------------------------------------------------
+def three():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
