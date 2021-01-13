34ebccd2f986e87d8903d0354f3a41d4ab4efde6
Robin Canela <rcanela1@gmail.com>
2021-01-13 00:37:26 -0500

    sec5.51.py complete

---
+++ source_code/day5/sec5.51.py
@@ -0,0 +1,15 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+
+for number in range(1, 11):
+    print(number)
+
+for number in range(1, 11, 3):
+    print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
