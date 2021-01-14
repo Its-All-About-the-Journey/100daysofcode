c588ab970b890d88ae58c9f79cb5850d305ac759
Eric Tapia <erictapia1@gmail.com>
2021-01-13 17:39:32 -0800

    Completed section 13.124.

---
+++ source_code/day013/sec13.124.py
@@ -0,0 +1,42 @@
+# Fix the Errors
+age = int( input("How old are you?") )
+if age > 18:
+    print(f"You can drive at age {age}.")
+
+
+# Print statement on line 4 has incorrect indentation.
+# Fix: Add 4 spaces
+
+# Print statement will not print age on line 4.
+# Fix: convert string to a f-string
+
+# Condition is trying to compare a string and integer.
+# Fix: 
+
+
+
+
+# STARTING CODE - UNTOUCHED
+############DEBUGGING#####################
+# # Fix the Errors
+# age = input("How old are you?")
+# if age > 18:
+# print("You can drive at age {age}.")
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+# #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#   b_list.append(new_item)
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
