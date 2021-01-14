beeadf8fe69d9c9e3126b6003c6c6a3dd7d8bce4
Eric Tapia <erictapia1@gmail.com>
2021-01-13 18:05:22 -0800

    Completed section 13.128.

---
+++ source_code/day013/sec13.128.py
@@ -0,0 +1,23 @@
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
+# Line 3 is using an assignment operator instead of equality.
+# Fix: Make it ==
+
+
+
+
+
+
+# STARTING CODE - UNTOUCHED
+#############################################################
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
