a848e9aca29610e03013cb1a982fdf5b6e0a656b
Eric Tapia <erictapia1@gmail.com>
2021-01-13 17:46:41 -0800

    Completed section 13.125.

---
+++ source_code/day013/sec13.125.py
@@ -0,0 +1,22 @@
+#Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
+
+# Line 5 is an comparison.
+# Fix: remove an = from ==
+
+
+# STARTING CODE - UNTOUCHED
+############DEBUGGING#####################
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
