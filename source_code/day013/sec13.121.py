500fca65bd86220c6af53cd3a58351c0a9585dac
Eric Tapia <erictapia1@gmail.com>
2021-01-13 17:15:07 -0800

    Completed section 121.

---
+++ source_code/day013/sec13.121.py
@@ -0,0 +1,19 @@
+############DEBUGGING#####################
+
+# Describe Problem
+def my_function():
+  for i in range(1, 21):
+    if i == 20:
+      print("You got it")
+my_function()  # Will not print because range is 1 - 19.  Fixed: change 20 to 21 on line 5.
+
+
+# STARTING CODE - UNTOUCHED
+############DEBUGGING#####################
+
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 20):
+#     if i == 20:
+#       print("You got it")
+# my_function()
