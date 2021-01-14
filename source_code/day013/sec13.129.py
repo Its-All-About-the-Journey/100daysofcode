85f483d5c920adf961be6e9e715595f066bd4660
Eric Tapia <erictapia1@gmail.com>
2021-01-13 18:09:35 -0800

    Completed section 13.129.

---
+++ source_code/day013/sec13.129.py
@@ -0,0 +1,34 @@
+year = int( input("Which year do you want to check?") )
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+
+# Year is a string and used in a modulos operation.
+# Fix: Convert str to an int
+
+
+
+
+
+# STARTING CODE - UNTOUCHED
+#############################################################
+# year = input("Which year do you want to check?")
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
