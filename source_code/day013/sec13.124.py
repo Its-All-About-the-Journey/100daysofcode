dfd105d9827f9515239f0109e15c9a0e5f214748
Eric Tapia <erictapia1@gmail.com>
2021-01-13 17:41:09 -0800

    Removed some of the 'STARTING CODE'.

--- source_code/day013/sec13.124.py
+++ source_code/day013/sec13.124.py
@@ -22,21 +22,3 @@ if age > 18:
 # age = input("How old are you?")
 # if age > 18:
 # print("You can drive at age {age}.")
-
-# #Print is Your Friend
-# pages = 0
-# word_per_page = 0
-# pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-# print(total_words)
-
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
-
-# mutate([1,2,3,5,8,13])
