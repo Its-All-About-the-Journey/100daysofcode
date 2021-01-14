9cf7bc31184a946a12c1e61195e54129ed499dd4
Adam Pawlowski <ajp26@acsu.buffalo.edu>
2021-01-14 18:38:10 -0500

    Day 14: Higher or Lower Game

--- source_code/day014/README.md
+++ source_code/day014/README.md
@@ -1,21 +1,61 @@
-
 # DAY 14
+
+Higher Lower Game
 
 # Description
 
-# Environment
-OS:
+A Higher or Lower Game
 
-Python version:
+* A subject is presented with the number of friendface followers they have.
+* You're presented with another sibject, and you have to guess if the number of followers it has, is higher or lower than the sample.
+* Keep going until you guess wrong - that's your score.
+* Play again, and see if you can beat the high score.
+
+# Environment
+OS: Ubuntu Bionic
+
+Python version: 3.6.9
 
 # Dependencies
 
+100 Days of Code Hypermanganate Utility Library (included)
+random
+
 # How to run script
 ```
-enter instructions here
+Call the script, and follow the game prompts to play.
+
+See how far you can get, then try and beat that record!
+
 ```
 
 # Sample output
 ```
-paste output here
+
+    __  ___       __
+   / / / (_)___ _/ /_  ___  _____
+  / /_/ / / __ `/ __ \/ _ \/ ___/
+ / __  / / /_/ / / / /  __/ /
+/_/ ///_/\__, /_/ /_/\___/_/
+   / /  /____/_      _____  _____
+  / /   / __ \ | /| / / _ \/ ___/
+ / /___/ /_/ / |/ |/ /  __/ /
+/_____/\____/|__/|__/\___/_/
+
+You're right! Current score: 1.
+Compare A: Dwayne Johnson, Actor and professional wrestler, from United States.
+
+ _    __
+| |  / /____
+| | / / ___/
+| |/ (__  )
+|___/____(_)
+
+Compare B: Justin Bieber, Musician, from Canada.
+Who has more followers? Choose a or b: <SPOILER>
+
+Sorry, that's not correct. Your score was: 1
+You've got the high score!
+High score is currently 1
+Would you like to play again? Please choose yes or no: no
 ```
---
+++ source_code/day014/main.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# 100 Days of Code : Day 14 Project
+# Higher Lower Game
+# Adam Pawlowski (@hypermanganate)
+
+from game_data import data
+from art import logo, vs
+from hundred_days_util import a_or_b, yes_or_no, clear_screen
+from random import randint
+
+# Const
+
+# Decl
+
+high_score = 0
+
+# Func
+
+def get_competitor(game_data_list):
+  """
+  Return a random dict of competitor data from the game data list.
+  Take advantage of the mutability of a list that I'd just learned about.
+  """
+
+  if not len(game_data_list):
+    return False
+  else:
+    return game_data_list.pop(randint(0, len(game_data_list) - 1))
+
+def setup(data):
+  """
+  Setup values for a new game.
+  """
+
+  game_data_list = data[:]
+  game_score = 0
+
+  return game_data_list, game_score
+
+def print_display(a, b, game_score):
+  """
+  Print the Higher or Lower Game Display
+  """
+
+  clear_screen()
+  print(logo)
+  if game_score:
+    print(f"""You're right! Current score: {game_score}.""")
+  print(f"""Compare A: {a['name']}, {a['description']}, from {a['country']}.""")
+  print(vs)
+  print(f"""Compare B: {b['name']}, {b['description']}, from {b['country']}.""")
+
+def get_selection():
+  """
+  Obtain and return the user's choice.
+  """
+
+  print("Who has more followers?", end=' ')
+  selection = a_or_b("A", "B")
+
+  return selection
+
+def get_winner(a, b):
+  """
+  Return True if the combatant beats the champion.
+  """
+
+  if b['follower_count'] > a['follower_count']:
+    return True
+
+  return False
+
+def do_lost_game(game_score):
+  """
+  Player has lost the game, display game over screen.
+  """
+
+  clear_screen()
+  print(f"""Sorry, that's not correct. Your score was: {game_score}""")
+
+def game(game_data_list, game_score):
+  """
+  Main Game Loop
+  """
+
+  if not len(game_data_list) >= 2:
+    print("Error, insufficient game data.")
+    exit()
+
+  # Initial Champion
+  champion = get_competitor(game_data_list)
+
+  correct = False
+
+  combatant = get_competitor(game_data_list)
+  print_display(champion, combatant, game_score)
+  winner = get_winner(champion, combatant)
+  selection = get_selection()
+
+  if selection == "A" and not winner:
+    correct = True
+  elif selection == "B" and winner:
+    correct = True
+
+  if not correct:
+    do_lost_game(game_score)
+    return False, game_score
+  else:
+    game_score += 1
+    return True, game_score
+
+if __name__ == '__main__':
+  while True:
+    game_data_list, game_score = setup(data)
+    game_state = True
+    while game_state:
+      game_state, game_score = game(game_data_list, game_score)
+    if game_score > high_score:
+      high_score = game_score
+      print("You've got the high score!")
+    print(f"""High score is currently {high_score}""")
+    print("Would you like to play again?", end=' ')
+    if not yes_or_no():
+      break
