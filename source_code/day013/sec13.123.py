3f2ee82512f34a0bbdd1ac00fcbcf2b1e41480b9
Eric Tapia <erictapia1@gmail.com>
2021-01-13 17:34:01 -0800

    Completed section 13.123.

---
+++ source_code/day013/sec13.123.py
@@ -0,0 +1,25 @@
+# Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1996:
+  print("You are a millenial.")
+elif year > 1995:
+  print("You are a Gen Z.")
+
+# Millenials are from 1981-1995 
+# Fix: change 1994 to 1996 on line 3 
+
+# Gen Z are from 1996 - present
+# Fix: change 1994 to 1995 on line 5
+
+
+
+
+
+# STARTING CODE - UNTOUCHED
+############DEBUGGING#####################
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year > 1994:
+#   print("You are a Gen Z.")
