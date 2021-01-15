05b202209163809d5d6df1d50cfeef7a723de77f
Eric Tapia <erictapia1@gmail.com>
2021-01-15 14:05:33 -0800

    Implemented the menu.

--- source_code/day015/coffee_machine.py
+++ source_code/day015/coffee_machine.py
@@ -9,8 +9,8 @@ def update_prompt():
 
 def prompt():
     # Get the user's input
-
-    return input(f"{ msg.ASK.format( menu() )}")
+    print( menu() )
+    return input(f"{msg.ASK_FOR_SELECTION}")
 
 def print_report():
     # Print current resource values
@@ -20,7 +20,21 @@ def print_report():
 
 def menu() -> str:
     # Return menu based on current resources
-    pass
+    menu = "Here is our current menu:"
+
+    # Build menu based on inventory availability
+    for item in MENU:
+        add_item = True
+        for key, value in MENU[item]["ingredients"].items():
+            if value > resources[key]:
+                add_item = False
+                break
+
+        if add_item:
+            menu = menu + f"\n   {item:<10} - $ {MENU[item]['cost']:.2f}"
+
+    return menu
+
 
 def currency_balance():
     # Return the currency balance
