36e32dd19716077a1109d71d87f09eba125553ca
grantarm <grantarm@buffalo.edu>
2021-01-15 13:17:54 -0500

    Day012 Number Guessing Game - Updated Code

--- source_code/day012/day012.py
+++ source_code/day012/day012.py
@@ -3,13 +3,13 @@
 # Day 12 Number Guessing Game
 
 from art import logo
-import random
+from random import randint
 
 
 print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100")
-number = random.randint(1, 100)
+number = randint(1, 100)
 print(f"Pssst, the correct answer is {number}")
 
 while True:
@@ -20,24 +20,25 @@ while True:
 		print("Invalid difficulty selection. Please try again...")
 
 guesses = 10 if difficulty == 'easy' else 5
-
+player_guess = None
 
 while True:
-	if guesses == 0:
+	if not guesses:
 		print("You've run out of guesses, you lose")
 		exit()
-	else:
-		print("Guess again.")
-	print(f"You have {guesses} attempts remaining to guess the number.")
-	player_guess = int(input("Make a guess: "))
-	if player_guess > number:
-		print("Too high.")
-		guesses -= 1
-		continue
-	elif player_guess < number:
-		print("Too low.")
-		guesses -= 1
-		continue
 	elif player_guess == number:
 		print(f"You got it! The answer was {number}")
 		exit()
+	else:
+		print("Guess again.")
+
+	print(f"You have {guesses} attempts remaining to guess the number.")
+	player_guess = int(input("Make a guess: "))
+
+	if player_guess > number:
+		print("Too high.")
+	elif player_guess < number:
+		print("Too low.")
+	guesses -= 1
+	continue
+
