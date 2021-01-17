b21a6a586c2c42a4712960fad927c6f9532e09fc
Eric Tapia <erictapia1@gmail.com>
2021-01-17 14:16:16 -0800

    Reduce a couple JSON objects to less than 120 characters per line.

--- source_code/day017/data.py
+++ source_code/day017/data.py
@@ -3,8 +3,10 @@ question_data = [
     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+             "you are free to take it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, "
+             "you are entitled to a state funeral.", "answer": "False"},
     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
