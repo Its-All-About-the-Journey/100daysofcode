e90352744398749bcd1616202d273f42af177b58
Robin Canela <rcanela1@gmail.com>
2021-01-18 22:59:50 -0700

    sec6.59 complete

---
+++ source_code/day6/sec6.59.py
@@ -0,0 +1,22 @@
+# Indentation
+# Function is like a folder
+
+# All code belongs to function
+def my_function():
+    print("Hello")
+    print("World")
+
+# Non-indented line does not belong to function
+def my_function1():
+    print("Hello")
+print("World")
+
+# Fnction block, IF block & ELIF block
+sky = "clear"
+def my_function2():
+    if sky == "clear":
+        print("blue")
+    elif sky == "cloudy":
+        print("grey")
+    print("Hello")
+print("World")
