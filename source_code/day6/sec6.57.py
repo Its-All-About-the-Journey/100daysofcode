2c2aa162ec2301b5d2826b4d0286c18c99f26ce5
Robin Canela <rcanela1@gmail.com>
2021-01-18 22:37:47 -0700

    sec6.57 complete

---
+++ source_code/day6/sec6.57.py
@@ -0,0 +1,26 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+# Reeborg's World Challenge
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# #Draw Square
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
