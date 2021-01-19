5eda040da919c7ca5e64cb3a826ca05c762a423b
Robin Canela <rcanela1@gmail.com>
2021-01-18 22:48:21 -0700

    6.2 updated

--- source_code/day6/6.1-hurdles_challenge_loop.py
+++ source_code/day6/6.1-hurdles_challenge_loop.py
@@ -21,3 +21,21 @@
 # jump()
 # jump()
 # jump()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# for step in range(6):
+#     jump()
