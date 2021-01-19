810c4067f9b5b6a5a5cc28799aa24c0ec41a74b3
Robin Canela <rcanela1@gmail.com>
2021-01-19 11:35:24 -0700

    sec6.60 complete

---
+++ source_code/day6/sec6.60.py
@@ -0,0 +1,67 @@
+# 🚨 For Loops 👇
+
+# for item in list_of_items:
+    #Do Something to each item
+
+# for number in range(a, b):
+    #print(number)
+
+# 🚨 While Loop 👇
+
+# while something_is_true
+    #Do Something repeteadly
+
+# while something_is_true
+    #Do this
+    #Then do this
+    #Then do this
+
+# 🚨 Reeborg's World, While Loop 👇
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+        
+# number_of_hurdles = 6
+# while number_of_hurdles > 0:
+#     jump()
+#     number_of_hurdles -= 1
+#     print(number_of_hurdles)
+
+# 🚨 Reeborg's World, While Loop to Goal 👇
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# 🚨 Reeborg's World, While Loop to Goal - 1st Example 👇
+
+# while not at_goal():
+#   jump()
+
+# 🚨 Reeborg's World, While Loop to Goal - 2nd Example 👇
+
+# while at_goal() != True:
+#   jump()
