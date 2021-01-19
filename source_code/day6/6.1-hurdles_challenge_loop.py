13d46ee71a86b6a8122a4d3cf525fad7d3a9cf2e
Robin Canela <rcanela1@gmail.com>
2021-01-18 22:46:34 -0700

    6.1 hurdles loop challenge complete

---
+++ source_code/day6/6.1-hurdles_challenge_loop.py
@@ -0,0 +1,23 @@
+# Hurdles Loop Challenge
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# jump()
+# jump()
+# jump()
+# jump()
+# jump()
+# jump()
