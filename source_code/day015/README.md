6fa90f2bd3334aa194a07ce7b5ca40a21208a9b9
grantarm <grantarm@buffalo.edu>
2021-01-19 16:38:59 -0500

    Day015 Coffee Machine - Initial Code and Comments

---
+++ source_code/day015/README.md
@@ -0,0 +1,50 @@
+
+# DAY 15
+
+# Description
+Coffee Machine Application
+
+# Environment
+OS: Windows 10
+
+Python version: 3.8
+
+# Dependencies
+None
+
+# How to run script
+```
+No Special Instructions
+```
+
+# Sample output
+```
+What would you like? Enter espresso/latte/cappuccino: espresso
+Please insert coins...
+Enter total number of quarters: 8
+Enter total number of dimes: 0
+Enter total number of nickels: 0
+Enter total number of pennies]: 0
+Here is $0.50 in change.
+Here is your espresso ☕ Enjoy!
+What would you like? Enter espresso/latte/cappuccino: report
+Water: 250ml
+Milk: 200ml
+Coffee: 82g
+Money: $1.50
+What would you like? Enter espresso/latte/cappuccino: latte
+Please insert coins...
+Enter total number of quarters: 20
+Enter total number of dimes: 2
+Enter total number of nickels: 7
+Enter total number of pennies]: 66
+Here is $3.71 in change.
+Here is your latte ☕ Enjoy!
+What would you like? Enter espresso/latte/cappuccino: report
+Water: 50ml
+Milk: 50ml
+Coffee: 58g
+Money: $4.00
+What would you like? Enter espresso/latte/cappuccino: exit
+Powering down...
+```
---
+++ source_code/day015/day015.py
@@ -0,0 +1,80 @@
+# Grant Armstrong
+# Day 15 Coffee Machine Application
+# 01/19/2021
+
+from data import MENU, resources
+
+
+# Function to print report of remaining resources
+def report():
+	print(f"Water: {resources['water']}ml")
+	print(f"Milk: {resources['milk']}ml")
+	print(f"Coffee: {resources['coffee']}g")
+	print(f"Money: ${resources['money']:,.2f}")
+
+
+# Function to check if there are sufficient resources
+def check_resources(selection):
+	drink = MENU[selection]
+	for ingredient, quantity in drink['ingredients'].items():
+		if quantity > resources[ingredient]:
+			print(f"Sorry there is not enough {ingredient}")
+			return False
+	return True
+
+
+# Function to deduct resources after making coffee
+def deduct_resources(selection):
+	drink = MENU[selection]
+	for ingredient, quantity in drink['ingredients'].items():
+		resources[ingredient] -= quantity
+
+
+# Function to prompt user for payment - returns amount user paid
+def payment(selection):
+	drink = MENU[selection]
+	print("Please insert coins...")
+	quarters = 0.25 * int(input("Enter total number of quarters: "))
+	dimes = 0.10 * int(input("Enter total number of dimes: "))
+	nickels = 0.05 * int(input("Enter total number of nickels: "))
+	pennies = 0.01 * int(input("Enter total number of pennies]: "))
+	total = quarters + dimes + nickels + pennies
+	cost = drink["cost"]
+	change = total - cost
+	if total < cost:
+		print("Sorry that's not enough money. Money refunded.")
+		return
+	elif total >= cost:
+		resources["money"] += cost
+		deduct_resources(selection)
+		print(f"Here is ${change:,.2f} in change.\nHere is your {selection} ☕ Enjoy!")
+		return
+
+
+# Function that ties everything together and runs the other functions
+def run_machine():
+	while True:
+		user_selection = input("What would you like? Enter espresso/latte/cappuccino: ").lower()
+
+		# Check to make sure the user entered a proper selection
+		if user_selection not in ['espresso', 'latte', 'cappuccino', 'report', 'exit']:
+			print("Invalid selection. Please try again...")
+			continue
+		# If the user selected report, show them a report
+		elif user_selection == 'report':
+			report()
+			continue
+		# If the user selected exit, exit the application
+		elif user_selection == 'exit':
+			print("Powering down...")
+			exit()
+
+		# Check to see if there are available resources for the users selection - run payment function if there are
+		if check_resources(user_selection):
+			payment(user_selection)  # payment function also runs the deduct_resources() function
+		# If there are insufficient resources, continue to the next iteration of the loop
+		else:
+			continue
+
+
+run_machine()
