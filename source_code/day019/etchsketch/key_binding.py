7f11b5c6089a681581803a854ed7247560f0258e
Eric Tapia <erictapia1@gmail.com>
2021-01-19 17:42:57 -0800

    Added key, function, and amount bindings.

---
+++ source_code/day019/etchsketch/key_binding.py
@@ -0,0 +1,8 @@
+class KeyBindings:
+    key_events = {
+        "w": { "function": "forward", "amount": 10},
+        "s": { "function": "backward", "amount": 10},
+        "a": { "function": "counter_clockwise", "amount": -5},
+        "d": { "function": "clockwise", "amount": 5},
+        "c": { "function": "clear", "amount": False}
+    }
