1c6f506f6ee9799d047dce5dab29fe20f4dfebde
grantarm <grantarm@buffalo.edu>
2021-01-21 16:42:16 -0500

    Day017 Quiz Project - Updated Questions

--- source_code/day017/main.py
+++ source_code/day017/main.py
@@ -2,7 +2,7 @@ from question_model import Question
 from data import question_data
 from quiz_brain import  QuizBrain
 
-question_bank = [Question(d['text'], d['answer']) for d in question_data]
+question_bank = [Question(d['question'], d['correct_answer']) for d in question_data]
 
 quiz = QuizBrain(question_bank)
 
