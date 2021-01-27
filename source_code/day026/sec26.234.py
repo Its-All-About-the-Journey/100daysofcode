0ee834ff16f34f82a33e3e073df0841104881fea
Eric Tapia <erictapia1@gmail.com>
2021-01-26 18:45:42 -0800

    Completed 26.234, even numbers.

---
+++ source_code/day026/sec26.234.py
@@ -0,0 +1,10 @@
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above
+
+#Write your 1 line code 👇 below:
+
+result = [number for number in numbers if not number % 2]
+
+#Write your code 👆 above:
+
+print(result)
