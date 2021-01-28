c49502c08cdce25737fc66c6e61671dd54a6cd1e
Robin Canela <rcanela1@gmail.com>
2021-01-27 21:49:24 -0800

    Day 8 complete

--- source_code/day8/8.6-caesar_cipher_pt4.py
+++ source_code/day8/8.6-caesar_cipher_pt4.py
@@ -10,13 +10,17 @@ def caesar(start_text, shift_amount, cipher_direction):
   if cipher_direction == "decode":
     shift_amount *= -1
   for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
     #TODO-3: What happens if the user enters a number/symbol/space?
     #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
     #e.g. start_text = "meet me at 3"
     #end_text = "•••• •• •• 3"
-    position = alphabet.index(char)
-    new_position = position + shift_amount
-    end_text += alphabet[new_position]
+
     
   print(f"Here's the {cipher_direction}d result: {end_text}")
 
@@ -27,13 +31,21 @@ print(art.logo)
 #e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
 #If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
 #Hint: Try creating a new function that calls itself if they type 'yes'. 
+should_end = True
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+while should_end:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
 
-#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-#Try running the program and entering a shift number of 45.
-#Hint: Think about how you can use the modulus (%).
+  #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+  #Try running the program and entering a shift number of 45.
+  #Hint: Think about how you can use the modulus (%).
+  shift = shift % 26
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_continue = False
+    print("Goodbye")
