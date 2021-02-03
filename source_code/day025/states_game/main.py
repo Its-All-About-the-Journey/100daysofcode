03a3adf53a95c0cfb880d802275e94d22ec2b284
grantarm <grantarm@buffalo.edu>
2021-02-03 16:35:15 -0500

    Day025 State Guessing Game - Updated Code

--- source_code/day025/states_game/main.py
+++ source_code/day025/states_game/main.py
@@ -14,7 +14,10 @@ data = pandas.read_csv('50_states.csv')
 game_on = True
 while game_on:
 	answer = screen.textinput(title=f'{len(states_guessed)}/50 States Correct', prompt='Enter a U.S. State:')
-	answer = answer.title()
+	try:
+		answer = answer.title()
+	except AttributeError:
+		break
 	if answer not in states_guessed:
 		if answer in data['state'].to_list():
 			state_row = data[data.state == answer].squeeze()
@@ -28,8 +31,16 @@ while game_on:
 	elif answer in states_guessed:
 		continue
 	if len(states_guessed) >= 50:
+		write.win_message()
 		game_on = False
 
-write.win_message()
-
-turtle.mainloop()
+# States to Learn
+missed_states_dict = {
+	'missed_states': []
+}
+if len(states_guessed) < 50:
+	for state in data.state.to_list():
+		if state not in states_guessed:
+			missed_states_dict['missed_states'].append(state)
+	missed_states = pandas.DataFrame(missed_states_dict)
+	missed_states.to_csv('missed_states.csv')
