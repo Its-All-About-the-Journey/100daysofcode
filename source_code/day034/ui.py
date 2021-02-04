1475f3f983982b0f82a24fcb71125e5463f90f78
Eric Tapia <erictapia1@gmail.com>
2021-02-04 07:41:05 -0800

    Completed section 34.305.

--- source_code/day034/ui.py
+++ source_code/day034/ui.py
@@ -1,11 +1,16 @@
 from tkinter import *
 
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self) -> None:
+    def __init__(self, quiz_brain: QuizBrain) -> None:
+        self.quiz = quiz_brain
+
+        # Window
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -15,7 +20,7 @@ class QuizInterface:
 
         # Canvas
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(150, 125, text="Question", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.question_text = self.canvas.create_text(150, 125, text="Question", fill=THEME_COLOR, font=("Arial", 20, "italic"), width=280)
 
         # Button Images
         btn_true_img = PhotoImage(file="./images/true.png")
@@ -31,7 +36,13 @@ class QuizInterface:
         self.btn_true.grid(row=2, column=0)
         self.btn_false.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self) -> None:
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
 
 
 if __name__ == "__main__":
