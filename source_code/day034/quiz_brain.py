1ca67b9119f25db8d157b01198959333b70ccbea
Eric Tapia <erictapia1@gmail.com>
2021-02-04 08:45:31 -0800

    Completed section 34.308.

--- source_code/day034/quiz_brain.py
+++ source_code/day034/quiz_brain.py
@@ -15,7 +15,7 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        q_text = html.unescape((self.current_question.text))
+        q_text = html.unescape(self.current_question.text)
 
         return f"Q.{self.question_number}: {q_text}"
 
@@ -24,11 +24,18 @@ class QuizBrain:
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
+
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
-        else:
-            print("That's wrong.")
+            return True
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+        return False
+
+        # if user_answer.lower() == correct_answer.lower():
+        #     self.score += 1
+        #     print("You got it right!")
+        # else:
+        #     print("That's wrong.")
+        #
+        # print(f"Your current score is: {self.score}/{self.question_number}")
+        # print("\n")
--- source_code/day034/ui.py
+++ source_code/day034/ui.py
@@ -27,8 +27,8 @@ class QuizInterface:
         btn_false_img= PhotoImage(file="./images/false.png")
 
         # Buttons
-        self.btn_true = Button(image=btn_true_img)
-        self.btn_false = Button(image=btn_false_img)
+        self.btn_true = Button(image=btn_true_img, command=self.check_answer_true)
+        self.btn_false = Button(image=btn_false_img, command=self.check_answer_false)
 
         # Grid Layout
         self.lbl_score.grid(row=0, column=1)
@@ -40,10 +40,36 @@ class QuizInterface:
 
         self.window.mainloop()
 
+    def check_answer(self, answer) -> None:
+        if self.quiz.check_answer(answer):
+            self.update_window_state("green", DISABLED)
+            self.update_score()
+
+        else:
+            self.update_window_state("red", DISABLED)
+
+        self.window.after(1000, func=self.get_next_question)
+
+    def check_answer_false(self) -> None:
+        self.check_answer("false")
+
+    def check_answer_true(self) -> None:
+        self.check_answer("true")
+
     def get_next_question(self) -> None:
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+            self.update_window_state("white")
 
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz.")
 
-if __name__ == "__main__":
-    quiz_ui = QuizInterface()
+    def update_score(self) -> None:
+        self.lbl_score.config(text=f"Score: {self.quiz.score}")
+
+    def update_window_state(self, color: str, state: str = NORMAL) -> None:
+        self.canvas.config(bg=color)
+        self.btn_true.config(state=state)
+        self.btn_false.config(state=state)
+
