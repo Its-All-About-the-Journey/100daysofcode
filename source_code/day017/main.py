4f91c94fc5f03b6a806cc74ec43a67e31b201fb6
Bill Masci <wmasci@gmail.com>
2021-04-20 12:56:32 -0700

    Day 17

--- source_code/day017/main.py
+++ source_code/day017/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -8,4 +9,10 @@ for question in question_data:
   new_question = Question(question_text, question_answer)
   question_bank.append(new_question)
 
-QuizBrain(question_bank)
+quiz = QuizBrain(question_bank)
+#quiz.next_question()
+
+while quiz.still_has_questions():
+  quiz.next_question()
+
+print(f"Your final score is {quiz.score} out of {len(quiz.question_list)}.")
