4d21b0f61ecea68239024869c0d9ca06fdc184f2
Michael Kutka <michael.kutka@gmail.com>
2021-06-14 16:43:20 -0400

    Day 1 of 100

---
+++ day1/input-exercise.py
@@ -0,0 +1,8 @@
+#Day 1.3 Exercise!!
+
+#First way I thought to do it without help
+name = input("What is your name? ")
+print(len(name))
+
+#Way I found to do it from searching google
+print(len(input("What is your name? ")))
---
+++ day1/main.py
@@ -0,0 +1,3 @@
+print("Day 1 - Python Print Function")
+print("The fucntion is declared like this:")
+print("print('what to print')")
