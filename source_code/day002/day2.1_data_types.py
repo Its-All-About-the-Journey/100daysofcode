c6c0ad5cb11c141ed54a392a059bf29ae5232d3a
sacgeek <43549331+sacgeek@users.noreply.github.com>
2023-03-13 23:40:27 -0700

    day2

---
+++ source_code/day002/day2.1_data_types.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# Find the first character from the input
+c1 = two_digit_number[0]
+# Find the second character from the input
+c2 = two_digit_number[1]
+# Convert the two characters to integers, add them, and print to the screen
+print(int(c1) + int(c2))
